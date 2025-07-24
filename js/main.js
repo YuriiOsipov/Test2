0f19df3df9cea7531faf2d9d75fe1ef5b87ab25a
Yurii Osipov <jeetforskill@gmail.com>
2025-07-24 16:48:38 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const button = document.querySelector(".button");
+
+button.addEventListener("click", showCards);
+
+async function showCards() {
+  const container = document.querySelector(".container");
+
+  try {
+    const request = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    const data = await request.json();
+
+    data.forEach((element) => {
+      const html = `<article class="card">
+               <div class="card__img">
+                 <img src="./img/${element.id}.gif" alt="profile-image" />
+               </div>
+               <div class="card-info">
+                 <p class="card__name">Name: ${element.name}</p>
+                 <p class="card__username">Username: ${element.username}</p>
+                 <p class="card__email">Email: ${element.email}</p>
+                 <p class="card__phone">Phone: ${element.phone}</p>
+                 <p class="card__website">Website: ${element.website}</p>
+               </div>
+             </article>`;
+
+      container.insertAdjacentHTML("afterbegin", html);
+    });
+  } catch (error) {
+    console.error(`Ошибка ${error.message}`);
+  }
+}
+
+console.log("Continue");
